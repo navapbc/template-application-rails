5c3102916326a3e098bcbd71bdc5c19bf01f2505
Rocket <rocketnova@users.noreply.github.com>
2024-04-09 14:53:25 -0700

    Add environment configuration

--- app-rails/config/environments/test.rb
+++ app-rails/config/environments/test.rb
@@ -5,6 +5,9 @@ require "active_support/core_ext/integer/time"
 # your test database is "scratch space" for the test suite and is wiped
 # and recreated between test runs. Don't rely on the data there!
 
+# Custom setting: set the default url.
+Rails.application.default_url_options[:host] = "localhost"
+
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
