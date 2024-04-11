cfb02a0ce2bb1f8181a4150f409a043757dd92c4
Rocket <rocketnova@users.noreply.github.com>
2024-04-11 14:58:53 -0700

    *Update download-and-install-template.sh

--- template-only-bin/download-and-install-template.sh
+++ template-only-bin/download-and-install-template.sh
@@ -1,22 +1,34 @@
 #!/usr/bin/env bash
 # -----------------------------------------------------------------------------
-# This script installs a template application to your project.
-# Run this script your project's root directory.
+# This script installs an application template to your project.
+# Run this script in your project's root directory.
+#
+# Positional parameters:
+#   TARGET_VERSION (optional) – the version of the template application to install.
+#     Defaults to main. Can be any target that can be checked out, including a branch,
+#     version tag, or commit hash.
 # -----------------------------------------------------------------------------
 set -euo pipefail
 
-TEMPLATE_NAME="template-application-nextjs"
+TARGET_VERSION=${1:-"main"}
+TEMPLATE_NAME="template-application-rails"
 
-echo "Fetch latest version of $(TEMPLATE_NAME)"
-git clone --single-branch --branch main --depth 1 git@github.com:navapbc/$(TEMPLATE_NAME).git
+git clone "https://github.com/navapbc/$TEMPLATE_NAME.git"
+cd "$TEMPLATE_NAME"
 
-echo "Install template"
-./$(TEMPLATE_NAME)/template-only-bin/install-template.sh $TEMPLATE_NAME
+echo "Checking out $TARGET_VERSION..."
+git checkout "$TARGET_VERSION"
+cd - &> /dev/null
 
-# Store template version in a file
-cd $(TEMPLATE_NAME)
-git rev-parse HEAD >../.$(TEMPLATE_NAME)-version
-cd -
+echo "Installing $TEMPLATE_NAME..."
+"./$TEMPLATE_NAME/template-only-bin/install-template.sh" "$TEMPLATE_NAME"
 
-echo "Clean up $(TEMPLATE_NAME) folder"
-rm -fr $(TEMPLATE_NAME)
+echo "Storing template version in a file..."
+cd "$TEMPLATE_NAME"
+git rev-parse HEAD >../".$TEMPLATE_NAME-version"
+cd - &> /dev/null
+
+echo "Cleaning up $TEMPLATE_NAME folder..."
+rm -fr "$TEMPLATE_NAME"
+
+echo "...Done."
