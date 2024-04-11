8458788b875ffa6e6df010dc11c88b1199a11429
Rocket <rocketnova@users.noreply.github.com>
2024-04-11 15:25:06 -0700

    *Update install-template.sh

--- template-only-bin/install-template.sh
+++ template-only-bin/install-template.sh
@@ -1,35 +1,37 @@
 #!/usr/bin/env bash
 # -----------------------------------------------------------------------------
-# This script installs a template application to your project.
-# Run this script your project's root directory.
+# This script installs an application template to your project.
+# Run this script using ./download-and-install-template.sh
+#
+# Positional parameters:
+#   TEMPLATE_NAME – the name of the template to install
 # -----------------------------------------------------------------------------
 set -euo pipefail
 
 CUR_DIR=$(pwd)
-TEMPLATE_NAME=$(basename $CUR_DIR)
 SCRIPT_DIR=$(dirname $0)
 TEMPLATE_DIR="$SCRIPT_DIR/.."
+TEMPLATE_NAME=$1
+# Use shell parameter expansion to get the last word, where the delimiter between
+# words is `-`.
+# See https://www.gnu.org/savannah-checkouts/gnu/bash/manual/bash.html#Shell-Parameter-Expansion
+TEMPLATE_SHORT_NAME="app-${TEMPLATE_NAME##*-}"
 
-echo "CUR_DIR: $CUR_DIR"
-echo "SCRIPT_DIR: $SCRIPT_DIR"
-echo "TEMPLATE_DIR: $TEMPLATE_DIR"
-echo "TEMPLATE_NAME: $TEMPLATE_NAME"
+echo "Copying files from $TEMPLATE_NAME..."
+cd $TEMPLATE_DIR
+# Note: Keep this list of paths in sync with INCLUDE_PATHS in update-template.sh
+# @TODO: Add .grype.yml
+cp -r \
+  .github \
+  .gitignore \
+  $TEMPLATE_SHORT_NAME \
+  docker-compose.yml \
+  init-postgres.sql \
+  docs \
+  $CUR_DIR
+cd - >& /dev/null
 
-# echo "Copy files from $(TEMPLATE_NAME)"
-# cd $TEMPLATE_DIR
-# # Note: Keep this list of paths in sync with INCLUDE_PATHS in update-template.sh
-# cp -r \
-#   .github \
-#   .gitignore \
-#   .grype.yml \
-#   app \
-#   docker-compose.yml \
-#   docs \
-#   $CUR_DIR
-# cd -
-
-# echo "Remove files relevant only to template development"
-# # Note: Keep this list of paths in sync with EXCLUDE_OPT in update-template.sh
-# rm .github/workflows/template-only-*
-# rm -rf .github/ISSUE_TEMPLATE
-# rm -rf docs/decisions/template
+echo "Removing files relevant only to template development..."
+# Note: Keep this list of paths in sync with EXCLUDE_OPT in update-template.sh
+rm -rf .github/workflows/template-only-*
+rm -rf .github/ISSUE_TEMPLATE
