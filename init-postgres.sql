e2668b46d09ac9a7d761c9b1270031d28b9fffe6
Rocket <rocketnova@users.noreply.github.com>
2024-04-11 14:06:02 -0700

    Add comment about database initialization script

--- init-postgres.sql
+++ init-postgres.sql
@@ -1,4 +1,6 @@
 -- This is run when you start the Docker postgres container.
--- Create separate databases for applications.
+-- Create separate databases for each application.
+-- This only runs if the postgresql data directory is empty.
+--   See the "Initialization scripts" section of https://hub.docker.com/_/postgres
 
 CREATE DATABASE app_rails;
