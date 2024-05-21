a25f7e3af15c5145d2637a555ba72e91f6c3f23e
Rocket <rocketnova@users.noreply.github.com>
2024-05-21 13:57:19 -0700

    *Prefix AWS environment variables

--- app-rails/config/environments/development.rb
+++ app-rails/config/environments/development.rb
@@ -36,7 +36,7 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
-  config.active_storage.service = if ENV["BUCKET_NAME"] then :amazon else :local end
+  config.active_storage.service = if ENV["AWS_BUCKET_NAME"] then :amazon else :local end
 
   config.action_mailer.delivery_method = if ENV["SES_EMAIL"] then :sesv2 else :letter_opener end
 
