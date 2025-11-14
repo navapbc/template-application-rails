76bb6f5d48dd75e0819d7dc0d3a2af88189dd8f7
Mike Giver <mikegiver@navapbc.com>
2025-11-14 13:30:12 -0500

    Set email_form in each action separately within each action; fix indentation

--- template/{{app_name}}/app/controllers/users/accounts_controller.rb
+++ template/{{app_name}}/app/controllers/users/accounts_controller.rb
@@ -2,14 +2,16 @@
 
 class Users::AccountsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_email_form
   before_action :set_password_form
   skip_after_action :verify_authorized
 
   def edit
+    @email_form = Users::UpdateEmailForm.new({ email: current_user.email })
   end
 
   def update_email
+    @email_form = Users::UpdateEmailForm.new(user_email_params)
+
     if @email_form.invalid?
       flash.now[:errors] = @email_form.errors.full_messages
       return render :edit, status: :unprocessable_content
@@ -27,20 +29,15 @@ class Users::AccountsController < ApplicationController
 
   private
 
-    def set_email_form
-      email_params = action_name == "update_email" ? user_email_params : { email: current_user.email }
-      @email_form = Users::UpdateEmailForm.new(email_params)
-    end
+  def set_password_form
+    @password_form = Users::ForgotPasswordForm.new({ email: current_user.email })
+  end
 
-    def set_password_form
-      @password_form = Users::ForgotPasswordForm.new({ email: current_user.email })
-    end
+  def auth_service
+    AuthService.new
+  end
 
-    def auth_service
-      AuthService.new
-    end
-
-    def user_email_params
-      params.require(:users_update_email_form).permit(:email)
-    end
+  def user_email_params
+    params.require(:users_update_email_form).permit(:email)
+  end
 end
