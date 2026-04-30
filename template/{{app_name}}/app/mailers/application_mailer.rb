28e7c7761b7dede5450dda8c4af060da29865242
jdettmannnava <145699825+jdettmannnava@users.noreply.github.com>
2026-04-30 06:44:19 -0700

    remove intervening variable

--- template/{{app_name}}/app/mailers/application_mailer.rb
+++ template/{{app_name}}/app/mailers/application_mailer.rb
@@ -2,8 +2,7 @@
 
 class ApplicationMailer < ActionMailer::Base
   # Make sure templates in app/views/overrides are used if present
-  @@mailer_overrides = ActionView::FileSystemResolver.new(Rails.root.join("app/views/overrides"))
-  prepend_view_path @@mailer_overrides
+  prepend_view_path ActionView::FileSystemResolver.new(Rails.root.join("app/views/overrides"))
 
   default from: ENV["AWS_SES_FROM_EMAIL"] || ENV["SES_EMAIL"]
   layout "mailer"
